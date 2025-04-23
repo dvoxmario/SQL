99d1e4679d8b7e01e785c6434e454de47d00ba2b
chaverradvox <chaverradvox@gmail.com>
2025-04-23 00:54:59 -0500

    primer sentencia select para aprender sql

---
+++ STATEMENT/select.sql
@@ -0,0 +1,9 @@
+
+SELECT * FROM users; // con esta sentencia llamamos a todos los datos de la tabla users.
+
+SELECT name FROM users; // con esta sentencia llamamos solo la columna name de la tabla users.
+
+SELECT user_id, name FROM users; // con esta sentencia llamamos dos columnas de la tabla users.
+
+
+
