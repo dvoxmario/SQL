00374a8d70a19f8fb48900aaea5c2c38d07847f6
chaverradvox <chaverradvox@gmail.com>
2025-04-23 01:15:10 -0500

    realizamos nuevas sentencias distinct y where.

---
+++ 02_distinct.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT * FROM users; // en esta sentencia miramos todos los datos distintos de la tabla por el *.
+
+SELECT DISTINCT name FROM users; // en esta sentencia especificamos mirar los datos distintos de la columna name.
+
+SLECT DISTINCT age FROM users; // en esta sentencia traemos todos los datos distintos de la columna age.
